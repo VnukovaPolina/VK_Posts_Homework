4957d9eedcfbbe47035fbe4ea20174fbdc6f4dc5
Polina Vnukova <vnukovapdeveloper@gmail.com>
2024-10-03 18:59:48 +0300

    Attachment classes added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,75 @@
 package ru.netology
 
-import ru.netology.WallService.newPostId
+interface Attachment {
+    val type : String
+}
+
+class Photo(val id : Int,
+            val ownerId : Int,
+            val photo_130 : String,
+            val photo_604 : String)
+
+class Video(val id : Int,
+            val ownerId : Int,
+            val title : String,
+            val duration : Int)
+
+class Audio(val id : Int,
+            val ownerId : Int,
+            val artist : String,
+            val title : String,
+            val duration : Int)
+
+class Poll(val id : Int,
+           val ownerId : Int,
+           val question : String,
+           val anonymous : Boolean)
+
+class Product(val id : Int,
+              val ownerId : Int,
+              val title : String,
+              val price : Int)
+
+class PhotoAttachment : Attachment {
+    override val type = "photo"
+    var photo = Photo(id = 1,
+        ownerId = 13240,
+        photo_130 = "https://vk.com/preview_link",
+        photo_604 = "https://vk.com/full_photo_link")
+}
+
+class VideoAttachment : Attachment {
+    override val type = "video"
+    var video = Video(id = 1,
+        ownerId = 24871,
+        title = "Cute cat",
+        duration = 75)
+}
+
+class AudioAttachment : Attachment {
+    override val type = "audio"
+    var audio = Audio(id = 1,
+        ownerId = 19652,
+        artist = "The Offspring",
+        title = "Pretty Fly (For A White Guy)",
+        duration = 184)
+}
+
+class PollAttachment : Attachment {
+    override val type = "poll"
+    var poll = Poll(id = 1,
+        ownerId = 72119,
+        question = "Do you love bacon with fried eggs?",
+        anonymous = false)
+}
+
+class ProductAttachment : Attachment {
+    override val type = "product"
+    var product = Product(id = 1,
+        ownerId = 28121,
+        title = "Pretty hat",
+        price = 1900)
+}
 
 data class Likes(var likesCount: Int = 0)
 
@@ -15,7 +84,8 @@ data class Post(
     val friendsOnly: Boolean = false,
     val likes: Likes,
     val reposts: Int = 0,
-    val views: Int
+    val views: Int,
+    val attachments: Array<Attachment> = emptyArray<Attachment>()
     )
 
 object WallService {
